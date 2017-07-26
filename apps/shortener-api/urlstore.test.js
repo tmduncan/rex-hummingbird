79ce66b0939c11174ab218b581a464647f91e8b0
Andy Terrel <aterrel@rexchange.com>
2017-07-26 18:10:21 -0500

    Enable linting on jest tests

--- apps/shortener-api/urlstore.test.js
+++ apps/shortener-api/urlstore.test.js
@@ -1,32 +1,21 @@
-/* eslint-disable */
-const urlstore = require("./urlstore");
-const redis = require("redis");
+const urlstore = require('./urlstore');
+const redis = require('redis');
 
-const redisUrl = process.env.REDIS_URL || "redis://redis:6379";
+const redisUrl = process.env.REDIS_URL || 'redis://redis:6379';
 const redisClient = redis.createClient(redisUrl);
 
 afterAll(() => {
   redisClient.quit();
 });
 
-test('urlstore can add and fetch data',() => {
+test('urlstore can add and fetch data', () => {
   expect.assertions(1);
   return urlstore
     .addShortUrl(redisClient, 'fb1', 'http://fb1.com')
-    .then(() => {
-      return urlstore.getShortUrl(redisClient, 'fb1');
-    })
-    .then(() => {
-      return urlstore.addShortUrl(redisClient, 'fb2', 'http://fb2.com');
-    })
-    .then(() => {
-      return urlstore.addShortUrl(redisClient, 'fb3', 'http://fb3.com');
-    })
-    .then(() => {
-      return urlstore.listShortUrls(redisClient);
-    })
-    .then(res => {
-      return expect(res).toBeTruthy();
-    })
-    .catch(e => { throw e });
+    .then(() => urlstore.getShortUrl(redisClient, 'fb1'))
+    .then(() => urlstore.addShortUrl(redisClient, 'fb2', 'http://fb2.com'))
+    .then(() => urlstore.addShortUrl(redisClient, 'fb3', 'http://fb3.com'))
+    .then(() => urlstore.listShortUrls(redisClient))
+    .then(res => expect(res).toBeTruthy())
+    .catch((e) => { throw e; });
 });
