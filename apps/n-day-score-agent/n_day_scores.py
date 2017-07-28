4e3d6887deadbed86d2f7b246a520a96695bb167
Andrew Barkett <abarkett@rexchange.com>
2017-07-27 19:34:46 -0700

    continuing re-organizing to put things in rex_utils

--- apps/n-day-score-agent/n_day_scores.py
+++ apps/n-day-score-agent/n_day_scores.py
@@ -1,8 +1,8 @@
-fom datetime import date, timedelta
-from rex_utils import RexDb
+from datetime import date, timedelta
+from rex_utils import RexDb, RexRedis
 
-client = redis.StrictRedis(host='dc1-stage-redis.ybasnj.0001.usw1.cache.amazonaws.com', port=6379, db=0)
-#gets openhouse visitors for the given listing_id
+rc = RexRedis.get_conn()
+
 def get_oh(listing_id, start, end):
     sql = 'SELECT visitors FROM open_house WHERE status = "COMPLETE" && scheduled_start_date > %s && scheduled_start_date < %s && listing_id = %s'
     rows = RexDb.select(sql, (start, end, listing_id))
@@ -17,7 +17,6 @@ def get_oh_nday(listing_id, days_ago):
     start -= days_ago
     return get_oh(listing_id, start, end)
 
-#gets showing visitors for the given listing_id
 def get_sh(listing_id, start, end):
     sql = 'SELECT COUNT(1) FROM showing WHERE showing_date > %s && showing_date < %s && listing_id = %s'
     rows = RexDb.select(sql, (start, end, listing_id))
@@ -27,42 +26,22 @@ def get_sh(listing_id, start, end):
 
 def get_sh_nday(listing_id, days_ago):
     end = date.today()
-    start = date.today()
     days_ago = timedelta(days=days_ago)
-    start -= days_ago
+    start = date.todau() - days_ago
     return get_sh(listing_id, start, end)
 
 def get_nday_numer(listing_id, days_ago):
     return get_sh_nday(listing_id, days_ago) + get_oh_nday(listing_id, days_ago)
 
 def get_nday_denom(listing_id, days_ago):
-    return client.get(str(days_ago) + "d-"+ "ga:pageviews-rex:listing_id-" + str(listing_id))
+    return rc.get(str(days_ago) + "d-"+ "ga:pageviews-rex:listing_id-" + str(listing_id))
 
 def get_nday_score(listing_id, days_ago):
     return get_nday_numer(listing_id, days_ago) / get_nday_denom(listing_id, days_ago)
 
-class RexDb:
-    cnx = None
-    
-    @staticmethod
-    def get_conn():
-        if RexDb.cnx is None or not RexDb.cnx.is_connected():
-            RexDb.cnx = mysql.connector.connect(user='root', password='password', host='db', port=3306, database='rex_db')
-        return RexDb.cnx
-
-    @staticmethod
-    def select(statement, val_tuple):
-        cnx = RexDb.get_conn()
-        cur = cnx.cursor()
-        cur.execute(statement, val_tuple)
-        rows = cur.fetchall()
-        cnx.close()
-        return rows
-
 def main():
-    start = date.today()
     days_ago = timedelta(days=15)
-    start -= days_ago
+    start date.today() - days_ago
     end = date.today()
     lid = 5
     print(get_oh(lid, str(start), str(end)))
