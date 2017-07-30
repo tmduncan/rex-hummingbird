7813e788c70db2d4226a8adcd4536d15a2735d40
Andy Terrel <aterrel@rexchange.com>
2017-07-30 13:25:07 -0500

    Adding more docker configuration and updating servers for mappings

--- apps/shortener-api/server.js
+++ apps/shortener-api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 const router = express.Router();
 
